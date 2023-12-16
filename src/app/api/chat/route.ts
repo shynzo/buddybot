c198cb6451327f28261e4a15c93d125cab318d8d
João Gustavo <gustavoddantas01@gmail.com>
2023-12-16 14:54:01 -0300

    refator(api/chat): changing ai plataform to fireworks.ai

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,28 +1,28 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import OpenAI from 'openai'
 
-// Optional, but recommended: run on the edge runtime.
-// See https://vercel.com/docs/concepts/functions/edge-functions
-export const runtime = 'edge'
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
+// Create an OpenAI API client (that's edge friendly!)
+// but configure it to point to fireworks.ai
+const fireworks = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+  baseURL: 'https://api.fireworks.ai/inference/v1',
 })
-
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
   const { messages } = await req.json()
 
-  // Request the OpenAI API for the response based on the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+  // Ask Fireworks for a streaming chat completion using Llama 2 70b model
+  // @see https://app.fireworks.ai/models/fireworks/llama-v2-70b-chat
+  const response = await fireworks.chat.completions.create({
+    model: 'accounts/fireworks/models/llama-v2-70b-chat',
     stream: true,
+    max_tokens: 1000,
     messages,
   })
-
-  // Convert the response into a friendly text-stream
+  // Convert the response into a friendly text-stream.
   const stream = OpenAIStream(response)
-
   // Respond with the stream
   return new StreamingTextResponse(stream)
 }
